7174d94b18b903f58d52e5e26af0198081f8d4d1
Gilbert Veitia Urbay <gveitia13@gmail.com>
2023-08-05 05:14:28 -0400

    ya

--- src/app/components/item/components/form-item/form-item.component.ts
+++ src/app/components/item/components/form-item/form-item.component.ts
@@ -1,10 +1,155 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {CategoryService} from "../../../../services/category.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ItemService} from "../../../../services/item.service";
+import {Category} from "../../../../models/category";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-form-item',
   templateUrl: './form-item.component.html',
   styleUrls: ['./form-item.component.css']
 })
-export class FormItemComponent {
+export class FormItemComponent implements OnInit {
+  myFrom: FormGroup
+  idItem: any
+  loading = true
+  action: string = 'Add';
+  loadingForm = false;
+  categories_list: Category[] = []
+
+  constructor(private fb: FormBuilder, private itemService: ItemService,
+              public snackBar: MatSnackBar, private aRoute: ActivatedRoute,
+              private router: Router, private categoryService: CategoryService) {
+    this.myFrom = this.fb.group({
+      name: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      code: ['', Validators.required],
+      categoryId: ['', Validators.required],
+    })
+    this.idItem = this.aRoute.snapshot.params['id']
+  }
+
+  saveItem() {
+    const formData = this.myFrom.value
+    this.myFrom.disable()
+    this.loadingForm = true
+    console.log(formData)
+    if (!this.idItem)
+      this.itemService.addItem(formData).subscribe({
+        next: (v: any) => {
+          this.myFrom.enable()
+          this.loadingForm = false
+          if (v.success) {
+            this.snackBar.open('The item has been created!', '', {
+              duration: 3000
+            })
+            this.router.navigate(['/item'])
+          } else {
+            this.snackBar.open(v.message, '', {
+              duration: 5000,
+            })
+          }
+        },
+        error: (e) => {
+          this.snackBar.open(e.message, '', {
+            duration: 5000,
+          })
+          this.myFrom.enable()
+          this.loadingForm = false
+        },
+        complete: () => {
+        }
+      })
+    else
+      this.itemService.editItem(formData, this.idItem).subscribe({
+        next: (v: any) => {
+          console.log(v)
+          this.myFrom.enable()
+          this.loadingForm = false
+          if (v.success) {
+            this.snackBar.open('The item has been updated!', '', {
+              duration: 3000
+            })
+            this.router.navigate(['/item'])
+          } else {
+            this.snackBar.open(v.message, '', {
+              duration: 5000,
+            })
+          }
+        },
+        error: (e) => {
+          this.snackBar.open(e.message, '', {
+            duration: 5000,
+          })
+          this.myFrom.enable()
+          this.loadingForm = false
+        },
+        complete: () => {
+        }
+      })
+  }
+
+  isEdit() {
+    this.itemService.getItem(this.idItem).subscribe({
+      next: (v: any) => {
+        this.myFrom.patchValue(v.data)
+        const categoryName = v.data.category
+
+        this.categoryService.getCategories().subscribe({
+          next: v => {
+            this.categories_list = v.items.slice()
+            console.log(this.categories_list)
+            const itemCategory = this.categories_list.find(c => c.name == categoryName)
+            this.myFrom.patchValue({categoryId: itemCategory?.id})
+            this.loading = false
+          },
+          error: e => {
+            this.loading = false
+            alert(e.message)
+          },
+          complete: () => {
+            this.loading = false
+          }
+        })
+
+      },
+      error: (e) => {
+        console.log(e)
+        this.loading = false
+      },
+      complete: () => {
+      }
+    })
+
+  }
+
+
+  ngOnInit(): void {
+
+    if (this.idItem !== undefined) {
+      this.action = 'Edit'
+      this.isEdit()
+    } else {
+      this.categoryService.getCategories().subscribe({
+        next: v => {
+          console.log(v)
+          this.categories_list = v.items.slice()
+          console.log(this.categories_list)
+          this.loading = false
+        },
+        error: e => {
+          this.loading = false
+          alert(e.message)
+        },
+        complete: () => {
+          this.loading = false
+        }
+      })
+    }
+  }
+
 
 }
--- src/app/components/item/item.module.ts
+++ src/app/components/item/item.module.ts
@@ -10,6 +10,8 @@ import {MatInputModule} from "@angular/material/input";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {MatTableModule} from "@angular/material/table";
 import {MatTooltipModule} from "@angular/material/tooltip";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatSelectModule} from "@angular/material/select";
 
 
 
@@ -28,6 +30,8 @@ import {MatTooltipModule} from "@angular/material/tooltip";
     MatProgressSpinnerModule,
     MatTableModule,
     MatTooltipModule,
+    ReactiveFormsModule,
+    MatSelectModule,
   ],
   exports:[
     ItemComponent
